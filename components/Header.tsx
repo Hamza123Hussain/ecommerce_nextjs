948ecbe27e3e1f558208b3d1bff9be63d4b3bb63
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-16 01:49:25 +0500

    Header Made

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+import LOGO from '../public/LOGO.webp'
+const Header = () => {
+  return (
+    <div className=" flex justify-between items-center bg-slate-300 shadow-md shadow-slate-800 px-4 py-2">
+      <Image
+        src={LOGO}
+        width={100}
+        height={100}
+        className="  rounded-full cursor-pointer "
+        alt="logo"
+      />
+      <h1 className="bg-gradient-to-r cursor-pointer hover:brightness-110 from-blue-500 to-purple-500 bg-clip-text text-6xl font-extrabold text-transparent">
+        TechTreasure
+      </h1>
+
+      <button className=" rounded-xl px-10 py-2 bg-green-500 hover:brightness-110 text-white border-2">
+        Login
+      </button>
+    </div>
+  )
+}
+
+export default Header
