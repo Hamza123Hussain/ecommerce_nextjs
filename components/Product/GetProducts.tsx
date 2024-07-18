b8b1bc0ef2e6af7e4ba1de994f7fd7ebaf9660dd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-19 01:00:05 +0500

    storing products data in local storage

--- components/Product/GetProducts.tsx
+++ components/Product/GetProducts.tsx
@@ -1,42 +1,58 @@
 'use client'
-import { getdata } from '@/functions/Product/Fetch'
+import React, { useEffect } from 'react'
+import { useAppContext } from '@/utils/Context'
 import { Product } from '@/utils/ProductInterface'
+import { getdata } from '@/functions/Product/Fetch'
+import ProductCard from './ProductCard'
 import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
 
 const GetProducts = () => {
-  const [productdata, setProduct] = useState<any>([])
+  const { products, setProducts } = useAppContext()
   const Router = useRouter()
   const fetchme = async () => {
-    const data: Product = await getdata()
-    console.log(data)
-    setProduct(data)
+    try {
+      const data: Product = await getdata()
+      setProducts(data)
+      localStorage.setItem('products', JSON.stringify(data))
+    } catch (error) {
+      console.error('Failed to fetch products:', error)
+    }
   }
+
   useEffect(() => {
     fetchme()
   }, [])
+  console.log('ALL PRODUCTS', products)
 
   return (
-    <div className=" grid grid-cols-2 justify-center items-center gap-10 p-4">
-      {productdata.map((element: Product, index: number) => {
-        return (
-          <div
-            key={element.id}
-            className=" flex flex-col gap-2 border-2 rounded-lg p-4 w-fit"
-          >
-            <img src={element.image_url} alt={element.name} className=" w-32" />
-
-            <h1>{element.name}</h1>
-            <h1>Rs {element.price}</h1>
-            <button
-              className=" bg-blue-600 rounded-lg p-4"
+    <div className="grid grid-cols-2 justify-center items-center gap-10 p-4">
+      {products?.map((element: any) => (
+        <div key={element.id}>
+          <div className=" flex flex-col ">
+            <img
               onClick={() => Router.push(`/Product/${element.id}`)}
-            >
-              Add To Cart
-            </button>
+              src={element.image_url}
+              alt={element.name}
+              className=" rounded-lg  hover:shadow-md hover:shadow-black"
+            />
+            <h1 className=" text-xl mt-5 text-slate-800 font-extrabold">
+              {element.name}
+            </h1>
+            <div className=" flex flex-col p-2">
+              {' '}
+              <h1 className=" text-sm text-gray-700">Description</h1>
+              <p className=" text-gray-500 text-xs p-2">
+                {element.description}
+              </p>
+            </div>
+
+            <div className=" flex gap-2 justify-end p-2  items-center">
+              <h1 className=" text-gray-700 font-bold text-sm">Price: </h1>
+              <h5 className=" text-gray-950 font-bold">Rs {element.price}</h5>
+            </div>
           </div>
-        )
-      })}
+        </div>
+      ))}
     </div>
   )
 }
