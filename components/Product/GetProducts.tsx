1093a22672195723c4c56a07aa8822bf4e6a1da0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-22 03:09:37 +0500

    Removed Product Card From Main Page

--- components/Product/GetProducts.tsx
+++ components/Product/GetProducts.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useAppContext } from '@/utils/Context'
 import { Product } from '@/utils/ProductInterface'
 import { getdata } from '@/functions/Product/Fetch'
-import ProductCard from './ProductCard'
+import { useRouter } from 'next/navigation'
 
 const GetProducts = () => {
   const { products, setProducts } = useAppContext()
@@ -20,6 +20,7 @@ const GetProducts = () => {
       setLoading(false)
     }
   }
+  const Router = useRouter()
 
   useEffect(() => {
     fetchme()
@@ -31,9 +32,33 @@ const GetProducts = () => {
 
   return (
     <div className="grid grid-cols-2 justify-center items-center gap-10 p-4">
-      {products?.map((element: Product) => (
-        <div key={element.id}>
-          <ProductCard productData={element} />
+      {products?.map((productData: Product) => (
+        <div key={productData.id}>
+          <div className=" flex flex-col ">
+            <img
+              onClick={() => Router.push(`/Product/${productData.id}`)}
+              src={productData.image_url}
+              alt={productData.name}
+              className=" rounded-lg  hover:shadow-md hover:shadow-black"
+            />
+            <h1 className=" text-xl mt-5 text-slate-800 font-extrabold">
+              {productData.name}
+            </h1>
+            <div className=" flex flex-col p-2">
+              {' '}
+              <h1 className=" text-sm text-gray-700">Description</h1>
+              <p className=" text-gray-500 text-xs p-2">
+                {productData.description}
+              </p>
+            </div>
+
+            <div className=" flex gap-2 justify-end p-2  items-center">
+              <h1 className=" text-gray-700 font-bold text-sm">Price: </h1>
+              <h5 className=" text-gray-950 font-bold">
+                Rs {productData.price}
+              </h5>
+            </div>
+          </div>
         </div>
       ))}
     </div>
