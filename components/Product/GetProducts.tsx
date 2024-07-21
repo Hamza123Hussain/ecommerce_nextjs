468df2bb5e8c7583c99318d31f5d6520968cf94f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-22 03:53:51 +0500

    added new component to show product details

--- components/Product/GetProducts.tsx
+++ components/Product/GetProducts.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { useAppContext } from '@/utils/Context'
 import { Product } from '@/utils/ProductInterface'
 import { getdata } from '@/functions/Product/Fetch'
-import { useRouter } from 'next/navigation'
+import ProductCard from './ProductCard'
+import HomeProduct from './HomeProduct'
 
 const GetProducts = () => {
   const { products, setProducts } = useAppContext()
@@ -20,45 +21,25 @@ const GetProducts = () => {
       setLoading(false)
     }
   }
-  const Router = useRouter()
 
   useEffect(() => {
     fetchme()
   }, [])
 
   if (loading) {
-    return <div>Loading...</div> // Display loading indicator while fetching data
+    return (
+      <div className=" flex gap-3 mt-52 justify-center items-center">
+        <div className="loader"></div> <div className="loader"></div>{' '}
+        <div className="loader"></div>{' '}
+      </div>
+    ) // Display loading indicator while fetching data
   }
 
   return (
-    <div className="grid grid-cols-2 justify-center items-center gap-10 p-4">
+    <div className="grid grid-cols-2 lg:grid-cols-4 justify-between gap-5 p-10 min-h-screen">
       {products?.map((productData: Product) => (
         <div key={productData.id}>
-          <div className=" flex flex-col ">
-            <img
-              onClick={() => Router.push(`/Product/${productData.id}`)}
-              src={productData.image_url}
-              alt={productData.name}
-              className=" rounded-lg  hover:shadow-md hover:shadow-black"
-            />
-            <h1 className=" text-xl mt-5 text-slate-800 font-extrabold">
-              {productData.name}
-            </h1>
-            <div className=" flex flex-col p-2">
-              {' '}
-              <h1 className=" text-sm text-gray-700">Description</h1>
-              <p className=" text-gray-500 text-xs p-2">
-                {productData.description}
-              </p>
-            </div>
-
-            <div className=" flex gap-2 justify-end p-2  items-center">
-              <h1 className=" text-gray-700 font-bold text-sm">Price: </h1>
-              <h5 className=" text-gray-950 font-bold">
-                Rs {productData.price}
-              </h5>
-            </div>
-          </div>
+          <HomeProduct product={productData} />
         </div>
       ))}
     </div>
