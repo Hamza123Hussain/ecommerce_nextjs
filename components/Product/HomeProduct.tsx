063f3d85acdcf489b6bf2fac69a9bd5c1822a4bd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-22 03:54:12 +0500

    new product details card for home page

---
+++ components/Product/HomeProduct.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Product } from '@/utils/ProductInterface'
+import { useRouter } from 'next/navigation'
+
+const HomeProduct = ({ product }: { product: Product }) => {
+  const Router = useRouter()
+  return (
+    <div key={product.id} onClick={() => Router.push(`/Product/${product.id}`)}>
+      <div className="relative hover:shadow-md hover:shadow-black flex w-full max-w-xs flex-col overflow-hidden rounded-lg  shadow-md">
+        <img
+          src={product.image_url}
+          alt={product.name}
+          className="rounded-t-lg  w-full h-[20vh] sm:h-[40vh]"
+        />
+
+        <div className="mt-4 px-5 pb-5">
+          <h5 className="text-xl tracking-tight text-slate-900">
+            {product.name}
+          </h5>
+
+          <div className="mt-2 mb-5 flex w-full ">
+            <span className="text-3xl font-bold text-slate-900">
+              Rs {product.price}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeProduct
