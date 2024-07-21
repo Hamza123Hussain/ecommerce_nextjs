53f04d2543b52f9547e996523a675854f10a989e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-22 03:11:31 +0500

    Spreading into smaller components

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,100 +1,61 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { useAppContext } from '@/utils/Context'
-import { Trash } from 'lucide-react'
+import Cartitem from '@/components/Cart/Cartitem'
+import CartDetails from '@/components/Cart/CartDetails'
 import { Product } from '@/utils/ProductInterface'
 
 const CartPage = () => {
-  const { cart, setCart, productData } = useAppContext()
-  const [loading, setLoading] = useState(true)
+  const { cart } = useAppContext()
+  const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
-    setLoading(false)
+    // This ensures that the component is only rendered on the client side
+    setIsClient(true)
   }, [])
 
-  const removeFromCart = (index: number, id: any) => {
-    console.log(productData)
-    const newCart = [...cart]
-    newCart.splice(index, 1)
-    setCart(newCart)
+  if (!isClient) {
+    // Return an empty div or loading state during server-side rendering
+    return <div>Loading...</div>
   }
 
-  const shipping = 250
+  const total = cart.reduce(
+    (total: any, element: Product) => {
+      total.totalprice = total.totalprice + element.price * element.quantity
+      total.totalquantity = total.totalquantity + element.quantity
+      return total
+    },
+    { totalprice: 0, totalquantity: 0 }
+  )
 
-  const totalPrice = cart.reduce((total: number, element: Product) => {
-    return total + element.price * element.quantity
-  }, 0)
-
-  console.log('TOTAL PRICE', totalPrice)
-  const totalquantity = cart.reduce((total: number, element: Product) => {
-    return total + element.quantity
-  }, 0)
-
-  const tax = (totalPrice * 16) / 100
-
-  if (loading) {
-    return <div>Loading...</div> // Display loading indicator while fetching data
-  }
+  const tax = (total.totalprice * 16) / 100
+  const shipping = tax + total.totalprice > 1500 ? 0 : 250
 
   return (
     <div className="container mx-auto mt-5 min-h-screen p-2 bg-gray-200">
-      <h1 className="text-3xl font-bold mb-4 text-center  ">Shopping Cart</h1>
+      <h1 className="text-3xl font-bold mb-4 text-center">Shopping Cart</h1>
       <div className="flex flex-col lg:flex-row gap-4">
         <div className="flex-1">
           {cart.length === 0 ? (
             <p className="text-lg text-center">Your cart is empty.</p>
           ) : (
-            cart.map((item: any, index: number) => (
+            cart.map((item: Product) => (
               <div
                 key={item.id}
                 className="flex flex-col md:flex-row items-center border-black hover:shadow-md hover:shadow-blue-100 gap-4 mb-4 p-4 border rounded-lg"
               >
-                <img
-                  src={item.image_url}
-                  alt={item.name}
-                  className="w-full md:w-20 h-20 object-cover"
-                />
-                <div className="flex-1 text-center md:text-left">
-                  <h2 className="text-xl font-semibold">{item.name}</h2>
-                  <p>Quantity: {item.quantity}</p>
-                  <p>Price: Rs{item.price}</p>
-                  <p>Total Price: Rs{item.price * item.quantity}</p>
-                </div>
-                <button
-                  onClick={() => removeFromCart(index, item.id)}
-                  className="text-red-600 hover:text-red-800"
-                >
-                  <Trash size={20} />
-                </button>
+                <Cartitem item={item} />
               </div>
             ))
           )}
         </div>
         {cart.length > 0 && (
-          <div className="w-full lg:w-1/3 bg-gray-100 px-2 py-4 mb-2 rounded-lg">
-            <h2 className="text-2xl font-bold mb-4 text-center lg:text-left">
-              Order Summary
-            </h2>
-            <div className="flex justify-between mb-2">
-              <span>Subtotal</span>
-              <span>{totalPrice.toFixed(2)}</span>
-            </div>
-            <div className="flex justify-between mb-2">
-              <span>Shipping</span>
-              <span>Rs {shipping.toFixed(2)}</span>
-            </div>
-            <div className="flex justify-between mb-2">
-              <span>Tax</span>
-              <span>Rs {tax.toFixed(2)}</span>
-            </div>
-            <div className="flex justify-between mb-2 font-bold">
-              <span>Total</span>
-              <span>Rs {(tax + totalPrice).toFixed(2)}</span>
-            </div>
-            <button className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600">
-              Checkout
-            </button>
-          </div>
+          <CartDetails
+            totalPrice={total.totalprice}
+            tax={tax}
+            shipping={shipping}
+            quantity={total.totalquantity}
+          />
         )}
       </div>
     </div>
