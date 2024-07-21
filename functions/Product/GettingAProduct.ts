c23665d4026f4d114c17e70e2051f27f5f415560
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-22 03:08:49 +0500

    removing unused code

--- functions/Product/GettingAProduct.ts
+++ functions/Product/GettingAProduct.ts
@@ -11,24 +11,8 @@ export const fetchProduct = async (id: string) => {
         const parsedProducts = JSON.parse(savedProducts)
         // Find the product by ID in the local storage products
         const product = parsedProducts.find((item: Product) => item.id === id)
-        const savedProduct = localStorage.getItem(`product-${id}`)
-        if (savedProduct) {
-          const savedData = JSON.parse(savedProduct)
-          product.quantity = savedData.quantity
-        }
-        if (product) {
-          return product
-        } else {
-          // Fetch product data from API if not found in local storage
-          const data: Product = await getdatabyid(id)
 
-          const savedProduct = localStorage.getItem(`product-${id}`)
-          if (savedProduct) {
-            const savedData = JSON.parse(savedProduct)
-            data.quantity = savedData.quantity
-          }
-          return data
-        }
+        return product
       }
     }
   } catch (error) {
