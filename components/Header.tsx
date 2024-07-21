e2d2d11f9d58dcf00f99853e841eaccd17ec1339
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-22 04:45:57 +0500

    added userbutton

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,10 @@ import React from 'react'
 import LOGO from '../public/LOGO.webp'
 import { ShoppingCart } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { UserButton, useUser } from '@clerk/nextjs'
 const Header = () => {
   const Router = useRouter()
+  const { user } = useUser()
   return (
     <div className=" flex flex-col sm:flex-row sm:justify-between sm:items-center bg-slate-300 shadow-md shadow-slate-800 sm:px-4 py-2 gap-5 sm:gap-0">
       <div
@@ -24,15 +26,22 @@ const Header = () => {
           TechTreasure
         </h1>
       </div>
-      <div className=" flex items-center justify-end gap-10">
+      <div className=" flex items-center justify-end gap-5">
         <ShoppingCart
           className=" cursor-pointer"
           onClick={() => Router.push('/cart')}
-          size={40}
+          size={50}
         />
-        <button className=" rounded-xl px-10 py-2 bg-green-500 hover:brightness-110 text-white border-2">
-          Login
-        </button>
+        {user ? (
+          <div className=" flex items-center gap-3 p-2">
+            <UserButton />
+            <h3> {user?.fullName}</h3>
+          </div>
+        ) : (
+          <button className=" rounded-xl px-10 py-2 bg-green-500 hover:brightness-110 text-white border-2">
+            Login
+          </button>
+        )}
       </div>
     </div>
   )
