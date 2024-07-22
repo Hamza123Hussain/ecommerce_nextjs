47e0aec845be0bdc60e3e788059b6eb1b37ec856
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-22 07:22:26 +0500

    array that stores type of input field data

---
+++ components/User/UserDetailsArray.ts
@@ -0,0 +1,50 @@
+export const fields = [
+  {
+    label: 'Name',
+    type: 'text',
+    name: 'name',
+    placeholder: 'Enter your name',
+  },
+  {
+    label: 'Phone Number',
+    type: 'tel',
+    name: 'phoneNumber',
+    placeholder: 'Enter your phone number',
+  },
+  {
+    label: 'Email',
+    type: 'email',
+    name: 'email',
+    placeholder: 'Enter your email',
+  },
+  {
+    label: 'Address',
+    type: 'text',
+    name: 'address',
+    placeholder: 'Enter your address',
+  },
+  {
+    label: 'City',
+    type: 'text',
+    name: 'city',
+    placeholder: 'Enter your city',
+  },
+  {
+    label: 'State',
+    type: 'text',
+    name: 'state',
+    placeholder: 'Enter your state',
+  },
+  {
+    label: 'Zip Code',
+    type: 'text',
+    name: 'zipCode',
+    placeholder: 'Enter your zip code',
+  },
+  {
+    label: 'Country',
+    type: 'text',
+    name: 'country',
+    placeholder: 'Enter your country',
+  },
+]
