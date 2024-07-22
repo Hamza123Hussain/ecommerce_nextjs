5f5b54ef142bdbb1946a615774015819e41d9710
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-23 02:36:55 +0500

    Api to Post User Details to Supabase

---
+++ app/api/User/Create/route.ts
@@ -0,0 +1,33 @@
+import { supabase } from '@/utils/Supabase'
+import { NextResponse } from 'next/server'
+
+export const POST = async (req: any) => {
+  try {
+    const payload = await req.json()
+
+    const { data, error } = await supabase
+      .from('Users')
+      .insert([
+        {
+          Name: payload?.name,
+          Email: payload?.email,
+          Phone: payload?.phoneNumber,
+          Address: payload?.address,
+          City: payload?.city,
+          State: payload?.state,
+          Country: payload?.country,
+          zipcode: payload?.zipCode,
+          created_at: new Date().toISOString(),
+        },
+      ])
+      .select()
+    if (data) {
+      return NextResponse.json({ data }, { status: 201 })
+    } else {
+      console.log(error)
+      return NextResponse.json({ error }, { status: 404 })
+    }
+  } catch (error) {
+    console.log('DATABASE ERROR', error)
+  }
+}
