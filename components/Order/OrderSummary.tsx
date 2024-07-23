0ad735608fdba29818b3bcb3f88be6b3af09d4b6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-24 02:07:35 +0500

    Using the Total Context

--- components/Order/OrderSummary.tsx
+++ components/Order/OrderSummary.tsx
@@ -1,33 +1,24 @@
 'use client'
 import { useAppContext } from '@/utils/Context'
-import { Product } from '@/utils/ProductInterface'
 import React from 'react'
 
 const OrderSummary: React.FC = () => {
-  const { cart } = useAppContext()
+  const { total } = useAppContext()
 
-  const totalPrice = cart.reduce(
-    (total: number, item: Product) => total + item.price * item.quantity,
-    0
-  )
-  const totalQuantity = cart.reduce(
-    (total: number, item: Product) => total + item.quantity,
-    0
-  )
   const shipping = 250
-  const tax = (totalPrice * 16) / 100
-  const grandTotal = totalPrice + shipping + tax
+  const tax = (total.totalprice * 16) / 100
+  const grandTotal = total.totalquantity + shipping + tax
 
   return (
     <div className="p-4 bg-gray-100 rounded-lg shadow-md mt-6">
       <h3 className="text-2xl font-bold mb-4 text-gray-700">Order Summary</h3>
       <div className="flex justify-between mb-2">
         <span>Total Quantity</span>
-        <span>{totalQuantity}</span>
+        <span>{total.totalquantity}</span>
       </div>
       <div className="flex justify-between mb-2">
         <span>Total Price</span>
-        <span>Rs{totalPrice.toFixed(2)}</span>
+        <span>Rs{total.totalprice.toFixed(2)}</span>
       </div>
       <div className="flex justify-between mb-2">
         <span>Shipping</span>
