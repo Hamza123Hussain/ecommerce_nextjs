34f5d973b8723f46b2ca6bade4e1e5d39d33163c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-23 05:16:38 +0500

    Order component mounted

---
+++ app/Order/page.tsx
@@ -0,0 +1,12 @@
+import OrderPage from '@/components/Order/OrderPage'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+      <OrderPage />
+    </>
+  )
+}
+
+export default Page
