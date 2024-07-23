783581297cd40565090795d31bfa0c30882ddc8e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-24 03:33:17 +0500

    Post order

---
+++ app/PostOrder/page.tsx
@@ -0,0 +1,12 @@
+import PostOrderPage from '@/components/Order/PostOrder/Main'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+      <PostOrderPage />
+    </>
+  )
+}
+
+export default Page
