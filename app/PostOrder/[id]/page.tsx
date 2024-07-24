7ce5916148569709d4f12759461f8ce2654ffab7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-25 04:01:50 +0500

    getting order data in frontend

--- app/PostOrder/[id]/page.tsx
+++ app/PostOrder/[id]/page.tsx
@@ -1,20 +1,21 @@
 'use client'
+import OrderDetails from '@/components/Order/PostOrder/OrdeItems'
 import OrderSum from '@/components/Order/PostOrder/Ordersummary'
 import UserDetails from '@/components/Order/PostOrder/UserDetails'
 import { GetOrder } from '@/functions/Order/GetOrder'
-import { useAppContext } from '@/utils/Context'
 import { Product } from '@/utils/ProductInterface'
 import React, { useEffect, useState } from 'react'
 
 const PostOrder = ({ params }: { params: any }) => {
   const [orderData, setOrderData] = useState<any>(null)
   const [isLoading, setIsLoading] = useState(true)
-  const { cart, setCart } = useAppContext()
+
   console.log(params.id)
 
   const fetchData = async () => {
     try {
       const data = await GetOrder(params.id)
+      console.log(data)
       setOrderData(data)
       setIsLoading(false)
     } catch (error) {
@@ -51,48 +52,32 @@ const PostOrder = ({ params }: { params: any }) => {
               Customer’s Cart
             </p>
 
-            {cart.map((item: Product) => (
+            {orderData.cart.map((item: Product) => (
               <div
                 key={item.id}
                 className="mt-4 md:mt-6 flex  flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full "
               >
-                <div className="pb-4 md:pb-8 w-full md:w-40">
-                  <img
-                    className="w-full hidden md:block"
-                    src={item.image_url}
-                    alt="product"
-                  />
-                  <img
-                    className="w-full md:hidden"
-                    src={item.image_url}
-                    alt="product"
-                  />
-                </div>
-                <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full  pb-8 space-y-4 md:space-y-0">
-                  <div className="w-full flex flex-col justify-start items-start space-y-8">
-                    <h3 className="text-xl xl:text-2xl font-semibold leading-6 text-gray-800">
-                      {item.name}
-                    </h3>
-                  </div>
-                  <div className="flex justify-between space-x-8 items-start w-full">
-                    <p className="text-base xl:text-lg leading-6">
-                      Rs{item.price}
-                      <span className="text-red-300 line-through"> $45.00</span>
-                    </p>
-                    <p className="text-base xl:text-lg leading-6 text-gray-800">
-                      {item.quantity}
-                    </p>
-                    <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">
-                      Rs{item.quantity * item.price}
-                    </p>
-                  </div>
-                </div>
+                <OrderDetails item={item} />
               </div>
             ))}
           </div>
-          <OrderSum />
+          <OrderSum
+            shipping={orderData.shipping}
+            total={orderData.total}
+            totalprice={orderData.totalprice}
+            tax={orderData.tax}
+          />
         </div>
-        <UserDetails />
+        <UserDetails
+          name={orderData.name}
+          email={orderData.email}
+          city={orderData.city}
+          state={orderData.state}
+          country={orderData.country}
+          address={orderData.address}
+          zipcode={orderData.zipcode}
+          phone={orderData.phone}
+        />
       </div>
     </div>
   )
