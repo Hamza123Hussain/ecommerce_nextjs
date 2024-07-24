5b7288bee163a6881c423267ac69416de5a5f7c8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-25 04:00:24 +0500

    sending props to relevant components

--- components/Order/PostOrder/Ordersummary.tsx
+++ components/Order/PostOrder/Ordersummary.tsx
@@ -3,19 +3,17 @@ import React from 'react'
 import { Product } from '@/utils/ProductInterface'
 import { useAppContext } from '@/utils/Context'
 
-const OrderSum = () => {
-  const { cart } = useAppContext()
-  const totalPrice = cart.reduce(
-    (total: number, item: Product) => total + item.price * item.quantity,
-    0
-  )
-  const totalQuantity = cart.reduce(
-    (total: number, item: Product) => total + item.quantity,
-    0
-  )
-  const tax = (totalPrice * 0.16).toFixed(2)
-  const shipping = 250
-
+const OrderSum = ({
+  shipping,
+  total,
+  totalprice,
+  tax,
+}: {
+  shipping: number
+  tax: number
+  total: number
+  totalprice: number
+}) => {
   return (
     <div className="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
       <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6   ">
@@ -25,7 +23,7 @@ const OrderSum = () => {
         <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
           <div className="flex justify-between  w-full">
             <p className="text-base leading-4 text-gray-800">Subtotal</p>
-            <p className="text-base leading-4 text-gray-600">Rs{totalPrice}</p>
+            <p className="text-base leading-4 text-gray-600">Rs{totalprice}</p>
           </div>
           {/* <div className="flex justify-between items-center w-full">
           <p className="text-base leading-4 text-gray-800">
@@ -52,7 +50,7 @@ const OrderSum = () => {
             Total
           </p>
           <p className="text-base font-semibold leading-4 text-gray-600">
-            Rs{totalPrice + tax + shipping}
+            Rs{total}
           </p>
         </div>
       </div>
