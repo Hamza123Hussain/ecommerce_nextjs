d42cdd8b56ac7fe8e083daea50ef9581a20da7ec
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-25 03:38:54 +0500

    fixing getting data function

--- functions/Order/GetOrder.ts
+++ functions/Order/GetOrder.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-export const OrderGet = async (id: any) => {
+export const GetOrder = async (id: any) => {
   try {
-    const Data = await axios.get('/api/Order/Get', id)
-    if (Data.status == 201) {
-      console.log('DATA RECIEVED', Data)
+    const Response = await axios.get(`/api/Order/Get?id=${id}`)
+    if (Response.status === 201) {
+      console.log('Response RECIEVED', Response.data[0])
     }
-    console.log('NO DATA')
+    console.log('NO Response')
   } catch (error) {
     console.log('API ERROR', error)
   }
