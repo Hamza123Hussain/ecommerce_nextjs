4687893528a738a0f8de8b2ccc2c78d4fab7e413
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-24 20:14:04 +0500

    Getting User Id

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+'use client'
 import AddProduct from '@/components/Product/Create'
 import GetProducts from '@/components/Product/GetProducts'
+import { useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
+  const { user } = useUser()
+  console.log('USERRRR', user?.id)
+
   return (
     <>
       {/* <AddProduct /> */}
