9e3c43915df96d78d8232a1a9626260cf3d47ee9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-24 20:30:04 +0500

    Add to cartbtn updated

--- components/Product/ProductCard.tsx
+++ components/Product/ProductCard.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { ArrowLeft } from 'lucide-react'
 import { Product } from '@/utils/ProductInterface'
 import { useRouter } from 'next/navigation'
-import AddtoCartBtn from '../Cart/AddtoCartBtn'
+import AddtoCartBtn from './AddtoCartBtn'
 
 const ProductCard = ({
   product,
