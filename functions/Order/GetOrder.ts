7c670104a6e4412f57db533dc4897c6b47df77a4
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-25 03:59:20 +0500

    Getting order data

--- functions/Order/GetOrder.ts
+++ functions/Order/GetOrder.ts
@@ -4,7 +4,7 @@ export const GetOrder = async (id: any) => {
   try {
     const Response = await axios.get(`/api/Order/Get?id=${id}`)
     if (Response.status === 201) {
-      console.log('Response RECIEVED', Response.data[0])
+      return Response.data[0]
     }
     console.log('NO Response')
   } catch (error) {
