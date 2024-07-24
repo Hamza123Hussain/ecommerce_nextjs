44823290c42d0c4d7ac70b928897f6ca13e1cb2d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-25 04:00:43 +0500

    fixing order item

--- components/Order/PostOrder/OrdeItems.tsx
+++ components/Order/PostOrder/OrdeItems.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Product } from '@/utils/ProductInterface'
 import React from 'react'
 
 interface OrderDetailsProps {
@@ -6,12 +7,37 @@ interface OrderDetailsProps {
   value: string
 }
 
-const OrderDetails: React.FC<OrderDetailsProps> = ({ label, value }) => {
+const OrderDetails = ({ item }: { item: Product }) => {
   return (
-    <div className="flex justify-between items-center mb-4">
-      <span className="font-semibold text-gray-600">{label}</span>
-      <span className="text-gray-900">{value}</span>
-    </div>
+    <>
+      <div className="pb-4 md:pb-8 w-full md:w-40">
+        <img
+          className="w-full hidden md:block"
+          src={item.image_url}
+          alt="product"
+        />
+        <img className="w-full md:hidden" src={item.image_url} alt="product" />
+      </div>
+      <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full  pb-8 space-y-4 md:space-y-0">
+        <div className="w-full flex flex-col justify-start items-start space-y-8">
+          <h3 className="text-xl xl:text-2xl font-semibold leading-6 text-gray-800">
+            {item.name}
+          </h3>
+        </div>
+        <div className="flex justify-between space-x-8 items-start w-full">
+          <p className="text-base xl:text-lg leading-6">
+            Rs{item.price}
+            <span className="text-red-300 line-through"> $45.00</span>
+          </p>
+          <p className="text-base xl:text-lg leading-6 text-gray-800">
+            {item.quantity}
+          </p>
+          <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">
+            Rs{item.quantity * item.price}
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
