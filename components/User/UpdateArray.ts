b8920fd2c256c7aeedd0d264674e57ea6ebc3753
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-25 19:47:18 +0500

    for fields

---
+++ components/User/UpdateArray.ts
@@ -0,0 +1,50 @@
+export const fields = [
+  {
+    label: 'Name',
+    type: 'text',
+    name: 'Name',
+    placeholder: 'Enter your name',
+  },
+  {
+    label: 'Phone Number',
+    type: 'tel',
+    name: 'Phone',
+    placeholder: 'Enter your phone number',
+  },
+  {
+    label: 'Email',
+    type: 'email',
+    name: 'Email',
+    placeholder: 'Enter your email',
+  },
+  {
+    label: 'Address',
+    type: 'text',
+    name: 'Address',
+    placeholder: 'Enter your address',
+  },
+  {
+    label: 'City',
+    type: 'text',
+    name: 'City',
+    placeholder: 'Enter your city',
+  },
+  {
+    label: 'State',
+    type: 'text',
+    name: 'State',
+    placeholder: 'Enter your state',
+  },
+  {
+    label: 'Zip Code',
+    type: 'text',
+    name: 'zipcode',
+    placeholder: 'Enter your zip code',
+  },
+  {
+    label: 'Country',
+    type: 'text',
+    name: 'Country',
+    placeholder: 'Enter your country',
+  },
+]
