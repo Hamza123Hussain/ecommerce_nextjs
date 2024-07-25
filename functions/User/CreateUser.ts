5362a37e64cc9a74f28d81821b66643fab7533be
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-25 18:33:43 +0500

    sending user id when creating user

--- functions/User/CreateUser.ts
+++ functions/User/CreateUser.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export const CreateUser = async (User: any) => {
+export const CreateUser = async (User: any, userid: any) => {
   try {
-    const Response = await axios.post('/api/User/Create', User)
+    const Response = await axios.post('/api/User/Create', { User, userid })
     if (Response.status == 201) {
       return Response.data[0]
     } else {
