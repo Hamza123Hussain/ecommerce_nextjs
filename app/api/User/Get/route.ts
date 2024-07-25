aab3a69b72a4216c9eb77af54af40dcd5b6bcb2a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-25 18:35:20 +0500

    api to get user details

---
+++ app/api/User/Get/route.ts
@@ -0,0 +1,21 @@
+import { supabase } from '@/utils/Supabase'
+import { NextResponse } from 'next/server'
+
+export const GET = async (req: any) => {
+  try {
+    const { searchParams } = new URL(req.url, `http://${req.headers.host}`)
+    const id = searchParams.get('id')
+
+    const { data, error } = await supabase
+      .from('Users')
+      .select('*')
+      .eq('userid', id)
+
+    if (data) {
+      return NextResponse.json(data, { status: 201 })
+    }
+    return NextResponse.json(error, { status: 404 })
+  } catch (error) {
+    return NextResponse.json({ message: `DATABASE ERROR ${error}` })
+  }
+}
