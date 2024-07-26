40c45db9cca7e576960760c2c0e7243e526ede05
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-27 02:25:55 +0500

    using context to get cost

--- components/Cart/CartDetails.tsx
+++ components/Cart/CartDetails.tsx
@@ -1,17 +1,9 @@
 'use client'
+import { useAppContext } from '@/utils/Context'
 import { useRouter } from 'next/navigation'
 import React from 'react'
-const CartDetails = ({
-  totalPrice,
-  shipping,
-  tax,
-  quantity,
-}: {
-  totalPrice: number
-  shipping: number
-  tax: number
-  quantity: number
-}) => {
+const CartDetails = () => {
+  const { total, tax, shipping, sum } = useAppContext()
   const Router = useRouter()
   return (
     <div className="w-full lg:w-1/3 bg-gray-100 p-4  mb-2 rounded-lg">
@@ -20,23 +12,23 @@ const CartDetails = ({
       </h2>
       <div className="flex justify-between mb-2">
         <span>Total Products </span>
-        <span>{quantity}</span>
+        <span>{total.totalquantity}</span>
       </div>
       <div className="flex justify-between mb-2">
         <span>Subtotal</span>
-        <span>{totalPrice.toFixed(2)}</span>
+        <span>{total.totalprice.toFixed(2)}</span>
       </div>
       <div className="flex justify-between mb-2">
         <span>Shipping</span>
-        <span>Rs {shipping.toFixed(2)}</span>
+        <span>Rs {shipping}</span>
       </div>
       <div className="flex justify-between mb-2">
         <span>Tax</span>
-        <span>Rs {tax.toFixed(2)}</span>
+        <span>Rs {tax}</span>
       </div>
       <div className="flex justify-between mb-2 font-bold">
         <span>Total</span>
-        <span>Rs {(tax + totalPrice + shipping).toFixed(2)}</span>
+        <span>Rs {sum}</span>
       </div>
       <button
         onClick={() => Router.push('/UserDetails')}
