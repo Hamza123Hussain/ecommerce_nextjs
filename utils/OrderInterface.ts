5b40bc0c39f0f9ba43ea1f4b0e58065f2e45da74
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-27 15:50:06 +0500

    Order Interface

---
+++ utils/OrderInterface.ts
@@ -0,0 +1,18 @@
+export interface Order {
+  cart: any[]
+  totalprice: number
+  totalquantity: number
+  tax: number
+  shipping: number
+  total: number
+  name: string
+  email: string
+  address: string
+  city: string
+  state: string
+  country: string
+  zipcode: string
+  userid: string
+  paymentmethod: string
+  phone: string
+}
