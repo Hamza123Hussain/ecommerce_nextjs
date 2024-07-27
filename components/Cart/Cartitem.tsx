8149247d9f2f1ca03194e616409640ad801e5961
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-27 17:16:51 +0500

    alert for item out of stock

--- components/Cart/Cartitem.tsx
+++ components/Cart/Cartitem.tsx
@@ -3,17 +3,26 @@ import useCartActions from '@/functions/Quantity/CustomQuantityHook'
 import { Product } from '@/utils/ProductInterface'
 import { Minus, Plus, Trash } from 'lucide-react'
 import React from 'react'
+import toast from 'react-hot-toast'
 
 const CartItems = ({ item }: { item: Product }) => {
   const { removeFromCart, increment, decrement } = useCartActions()
 
+  const handleIncrement = () => {
+    if (item.stock > 0) {
+      increment(item.id)
+    } else {
+      toast.error('Item is out of stock')
+    }
+  }
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-12  gap-6 items-center border p-4 rounded-lg mb-4 bg-white shadow-md">
+    <div className="grid grid-cols-1 md:grid-cols-12 gap-6 items-center border p-4 rounded-lg mb-4 bg-white shadow-md">
       <div className="col-span-6 md:col-span-4 flex justify-center">
         <img
           src={item.image_url}
           alt={item.name}
-          className=" w-[30vh]  h-[30vh]   rounded-t-lg md:rounded-xl"
+          className="w-[30vh] h-[30vh] rounded-t-lg md:rounded-xl"
         />
       </div>
       <div className="col-span-6 md:col-span-3">
@@ -23,8 +32,8 @@ const CartItems = ({ item }: { item: Product }) => {
           Total Price: Rs{item.price * item.quantity}
         </p>
       </div>
-      <div className="col-span-4 flex items-center justify-center  md:justify-between ">
-        <div className=" flex justify-center items-center">
+      <div className="col-span-4 flex items-center justify-center md:justify-between">
+        <div className="flex justify-center items-center">
           <button
             onClick={() => decrement(item.id)}
             className="bg-gray-200 p-2 rounded-full hover:bg-gray-300 transition-colors"
@@ -33,14 +42,13 @@ const CartItems = ({ item }: { item: Product }) => {
           </button>
           <span className="mx-2 text-lg">{item.quantity}</span>
           <button
-            onClick={() => increment(item.id)}
+            onClick={handleIncrement}
             className="bg-gray-200 p-2 rounded-full hover:bg-gray-300 transition-colors"
           >
             <Plus size={16} />
           </button>
         </div>
       </div>
-
       <button
         onClick={() => removeFromCart(item.id)}
         className="text-red-600 hover:text-red-800"
