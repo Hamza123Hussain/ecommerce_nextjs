0cfaf6ebde3e552c01c9c1db1bca2e54ce233b3b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-27 16:01:00 +0500

    converting code into a component

--- components/Order/PostOrder/UserDetails.tsx
+++ components/Order/PostOrder/UserDetails.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useAppContext } from '@/utils/Context'
 import React from 'react'
+import UserAddress from './UserAddres'
 
 const UserDetails = () => {
   const { userDetail } = useAppContext()
@@ -52,30 +53,7 @@ const UserDetails = () => {
             <p className="text-sm text-gray-700">{userDetail.Phone}</p>
           </div>
         </div>
-        <div className="border-t border-gray-200 pt-4">
-          <p className="text-xl font-semibold text-gray-900">
-            Shipping Address
-          </p>
-          <p className="text-sm text-gray-700 mt-2">{userDetail.Address}</p>
-          <div className="grid grid-cols-2 gap-4 mt-4">
-            <div>
-              <p className="text-lg font-semibold text-gray-800">Country</p>
-              <p className="text-sm text-gray-600">{userDetail.Country}</p>
-            </div>
-            <div>
-              <p className="text-lg font-semibold text-gray-800">City</p>
-              <p className="text-sm text-gray-600">{userDetail.City}</p>
-            </div>
-            <div>
-              <p className="text-lg font-semibold text-gray-800">State</p>
-              <p className="text-sm text-gray-600">{userDetail.State}</p>
-            </div>
-            <div>
-              <p className="text-lg font-semibold text-gray-800">Zipcode</p>
-              <p className="text-sm text-gray-600">{userDetail.Zipcode}</p>
-            </div>
-          </div>
-        </div>
+        <UserAddress userDetail={userDetail} />
       </div>
     </div>
   )
