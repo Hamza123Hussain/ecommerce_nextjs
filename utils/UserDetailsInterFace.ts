f5e13942b733d261a1da04b64c2f1c4e8c8c0c20
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-27 15:49:57 +0500

    UserDetails Interface

---
+++ utils/UserDetailsInterFace.ts
@@ -0,0 +1,12 @@
+export interface User {
+  name: string
+  email: string
+  phoneNumber: string
+  address: string
+  city: string
+  state: string
+  country: string
+  zipCode: string
+  userid: string
+  createdAt: string
+}
