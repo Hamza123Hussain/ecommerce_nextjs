c439fe6c2978238d6842b93fabc6a0df26f6caff
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-03 17:18:51 +0500

    return true if product created

--- app/api/Product/ADD/route.ts
+++ app/api/Product/ADD/route.ts
@@ -5,21 +5,24 @@ export const POST = async (req: any) => {
   try {
     const payload = await req.json()
 
-    const { data, error } = await supabase.from('products').insert([
-      {
-        name: payload?.Product.name,
-        description: payload?.Product.description,
-        price: parseFloat(payload?.Product.price.toString()),
-        stock: parseInt(payload?.Product.stock.toString()),
-        category: payload?.Product.category,
-        image_url: payload?.Product.image_url,
-        quantity: payload?.Product.quantity,
-        created_at: new Date().toISOString(),
-        updated_at: new Date().toISOString(),
-      },
-    ])
+    const { data, error } = await supabase
+      .from('products')
+      .insert([
+        {
+          name: payload?.Product.name,
+          description: payload?.Product.description,
+          price: parseFloat(payload?.Product.price.toString()),
+          stock: parseInt(payload?.Product.stock.toString()),
+          category: payload?.Product.category,
+          image_url: payload?.Product.image_url,
+          quantity: payload?.Product.quantity,
+          created_at: new Date().toISOString(),
+          updated_at: new Date().toISOString(),
+        },
+      ])
+      .select()
     if (data) {
-      return NextResponse.json({ data }, { status: 201 })
+      return NextResponse.json(true, { status: 201 })
     } else {
       return NextResponse.json({ error }, { status: 404 })
     }
