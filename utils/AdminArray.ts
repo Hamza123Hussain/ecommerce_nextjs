05f1cd64642fc843eef499d068cdd044d30739fe
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-03 16:18:54 +0500

    Adding Href instead of Componet

--- utils/AdminArray.ts
+++ utils/AdminArray.ts
@@ -1,22 +1,10 @@
-import React from 'react'
-import Products from '@/components/Admin/Products'
-import AddProduct from '@/components/Admin/AddProduct'
-import TotalRevenue from '@/components/Admin/TotalRevenue'
-import AllOrders from '@/components/Admin/AllOrders'
-import Users from '@/components/Admin/Users'
-
-// Define the type for the admin array objects
-type AdminItem = {
-  id: number
-  Name: string
-  Component: React.ComponentType
-}
+import { Admin } from './AdminInterface'
 
 // Create the array using the correct types
-export const AdminArray: AdminItem[] = [
-  { id: 1, Name: 'Products', Component: Products },
-  { id: 2, Name: 'Add Product', Component: AddProduct },
-  { id: 3, Name: 'Total Orders', Component: AllOrders },
-  { id: 4, Name: 'Total Revenue', Component: TotalRevenue },
-  { id: 5, Name: 'Users', Component: Users },
+export const AdminArray: Admin[] = [
+  { id: 1, Name: 'Products', Href: 'Products' },
+  { id: 2, Name: 'Add Product', Href: 'AddProduct' },
+  { id: 3, Name: 'Total Orders', Href: 'AllOrders' },
+  { id: 4, Name: 'Total Revenue', Href: 'TotalRevenue' },
+  { id: 5, Name: 'Users', Href: 'Users' },
 ]
