2ad735309b26cdee0121791ba30a2fa59c39a975
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-03 17:02:36 +0500

    Api to Get All Orders

---
+++ app/api/Order/GetAll/route.ts
@@ -0,0 +1,15 @@
+import { supabase } from '@/utils/Supabase'
+import { NextResponse } from 'next/server'
+
+export const GET = async (req: any) => {
+  try {
+    const { data, error } = await supabase.from('orders').select('*')
+
+    if (data) {
+      return NextResponse.json(data, { status: 201 })
+    }
+    return NextResponse.json({ error }, { status: 400 })
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 })
+  }
+}
