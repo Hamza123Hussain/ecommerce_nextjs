f17c91dc1dcb8b2c3ee9563c46c764884a3916e0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-03 20:06:07 +0500

    Update Route Made

---
+++ app/api/Product/Update/route.ts
@@ -0,0 +1,32 @@
+import { supabase } from '@/utils/Supabase'
+import { NextResponse } from 'next/server'
+
+export const POST = async (req: any) => {
+  try {
+    const payload = await req.json()
+
+    const { data, error } = await supabase
+      .from('products')
+      .update([
+        {
+          name: payload?.Product.name,
+          description: payload?.Product.description,
+          price: parseFloat(payload?.Product.price.toString()),
+          stock: parseInt(payload?.Product.stock.toString()),
+          category: payload?.Product.category,
+          image_url: payload?.Product.image_url,
+          quantity: payload?.Product.quantity,
+
+          updated_at: new Date().toISOString(),
+        },
+      ])
+      .select()
+    if (data) {
+      return NextResponse.json(true, { status: 201 })
+    } else {
+      return NextResponse.json({ error }, { status: 404 })
+    }
+  } catch (error) {
+    console.log('DATABASE ERROR', error)
+  }
+}
