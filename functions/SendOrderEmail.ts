9a591820050fec6a398aaacac3634c42450af56a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-03 06:43:44 +0500

    Sending to Api For Email Template

--- functions/SendOrderEmail.ts
+++ functions/SendOrderEmail.ts
@@ -1,13 +1,35 @@
 import axios from 'axios'
 
-export const SendEmail = async (Email: any, Subject: string, Text: string) => {
+export const SendEmail = async (
+  Email: any,
+  Fullname: any,
+  Name: any,
+  cart: Array<{ name: string; quantity: number; price: number }>,
+  total: number,
+  address: string,
+  paymentMethod: string,
+  Subject: string
+) => {
   try {
-    const Response = await axios.post('/api/Email', { Email, Subject, Text })
+    const response = await axios.post('/api/Email', {
+      Email,
+      Fullname,
+      Name,
+      cart,
+      total,
+      address,
+      paymentMethod,
+      Subject,
+    })
 
-    if (Response.status == 200) {
+    if (response.status === 200) {
       return true
     }
-  } catch (error) {
-    console.log('ERROR IN FUNCTION', error)
+  } catch (error: any) {
+    console.error(
+      'ERROR IN FUNCTION',
+      error.response ? error.response.data : error.message
+    )
+    return false
   }
 }
