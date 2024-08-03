54e1bca4a987475ecaeae66131ac00e66d063818
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-03 17:03:09 +0500

    Component to Get All Orders

--- app/Admin/AllOrders/page.tsx
+++ app/Admin/AllOrders/page.tsx
@@ -1,5 +1,50 @@
-import React from 'react'
+'use client'
+import { GetuserOrder } from '@/functions/Order/GetOrderByID'
+import React, { useEffect, useState } from 'react'
+import UserOrder from '@/components/Order/UserOrder'
+import { useAppContext } from '@/utils/Context'
+import { Order } from '@/utils/OrderInterface'
+import { GetAllOrders } from '@/functions/Order/GetAllOrders'
 
-export default function AllOrders() {
-  return <div></div>
+const MyOrders = () => {
+  const [orders, setOrders] = useState([])
+  const { loading, setLoading } = useAppContext()
+  const GetData = async () => {
+    const data = await GetAllOrders()
+    if (data) {
+      setOrders(data)
+      console.log('USER ORDER ', data)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    GetData()
+  }, [])
+
+  if (loading) {
+    return (
+      <div className=" min-h-screen flex justify-center items-center">
+        <div className="loader"></div>
+      </div>
+    )
+  }
+  return (
+    <div className="min-h-screen p-6 bg-white">
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">
+        ALL Orders
+      </h1>
+      {orders.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {orders.map((order: Order) => (
+            <UserOrder key={order.id} order={order} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-lg text-gray-700">No orders found.</p>
+      )}
+    </div>
+  )
 }
+
+export default MyOrders
