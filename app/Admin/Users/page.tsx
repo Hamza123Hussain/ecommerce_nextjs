9ae611f3b7329d4932f05b84332c6a2e080e8f10
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-04 01:39:56 +0500

    adding a loader and mounting user card

--- app/Admin/Users/page.tsx
+++ app/Admin/Users/page.tsx
@@ -1,9 +1,13 @@
 'use client'
+import UserCardForAdmin from '@/components/User/UserCardForAdmin'
 import { GetAllUsers } from '@/functions/User/GetAllUser'
-import React, { useEffect, useState } from 'react'
+import { UserDetails } from '@/utils/UserDetailInterface'
+import React, { use, useEffect, useState } from 'react'
+import { ClipLoader } from 'react-spinners'
 
 const Page = () => {
   const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const GetUsers = async () => {
     try {
@@ -14,6 +18,8 @@ const Page = () => {
       }
     } catch (error) {
       console.log('UNABLE TO FETCH', error)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -24,41 +30,19 @@ const Page = () => {
   return (
     <div className="min-h-screen flex flex-col items-center bg-gray-100 py-10">
       <h1 className="text-4xl font-bold mb-10">User List</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
-        {users.map((user: any) => (
-          <div
-            key={user.id}
-            className="bg-white text-xs sm:text-sm shadow-lg rounded-lg overflow-hidden p-6"
-          >
-            <h2 className="text-2xl font-semibold mb-2">{user.Name}</h2>
-            <p className="text-gray-700 mb-2">
-              <span className="font-semibold">Email:</span> {user.Email}
-            </p>
-            <p className="text-gray-700 mb-2">
-              <span className="font-semibold">Phone:</span> {user.Phone}
-            </p>
-            <p className="text-gray-700 mb-2">
-              <span className="font-semibold">Address:</span> {user.Address}
-            </p>
-            <p className="text-gray-700 mb-2">
-              <span className="font-semibold">City:</span> {user.City}
-            </p>
-            <p className="text-gray-700 mb-2">
-              <span className="font-semibold">State:</span> {user.State}
-            </p>
-            <p className="text-gray-700 mb-2">
-              <span className="font-semibold">Country:</span> {user.Country}
-            </p>
-            <p className="text-gray-700 mb-2">
-              <span className="font-semibold">Zipcode:</span> {user.zipcode}
-            </p>
-            <p className="text-gray-500 text-sm">
-              <span className="font-semibold">Joined:</span>{' '}
-              {new Date(user.created_at).toLocaleDateString()}
-            </p>
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <ClipLoader size={50} color={'#123abc'} loading={loading} />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
+          {users.map((user: UserDetails) => (
+            <>
+              <UserCardForAdmin key={user.id} user={user} />
+            </>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
