f755ffda10dd6ed37f20ac9571d3e258f175377b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-03 16:03:08 +0500

    giving margin

--- app/Admin/page.tsx
+++ app/Admin/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Admin = () => {
   return (
-    <div className=" flex flex-col ">
+    <div className=" flex flex-col my-5 ">
       <h1 className=" text-center text-3xl text-slate-400">Admin DashBoard</h1>
+      <div className=" grid grid-cols-1 sm:grid-cols-2"></div>
     </div>
   )
 }
