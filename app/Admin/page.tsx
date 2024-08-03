b12493a78494b160faa065e541d5c4dc1270a66e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-03 15:33:03 +0500

    Admin Deployment Started

---
+++ app/Admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className=" flex flex-col ">
+      <h1 className=" text-center text-3xl text-slate-400">Admin DashBoard</h1>
+    </div>
+  )
+}
+
+export default Admin
