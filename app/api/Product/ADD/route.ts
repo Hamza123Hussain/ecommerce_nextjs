6c81befc03c34c693e360028e394fe4534f46a11
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-04 19:45:23 +0500

    changing the api according to firebase

--- app/api/Product/ADD/route.ts
+++ app/api/Product/ADD/route.ts
@@ -1,32 +1,29 @@
-import { supabase } from '@/utils/Supabase'
+// src/pages/api/Product/ADD.js
+import { db } from '@/utils/FireBaseConfig'
+import { collection, addDoc } from 'firebase/firestore'
 import { NextResponse } from 'next/server'
 
 export const POST = async (req: any) => {
   try {
     const payload = await req.json()
 
-    const { data, error } = await supabase
-      .from('products')
-      .insert([
-        {
-          name: payload?.Product.name,
-          description: payload?.Product.description,
-          price: parseFloat(payload?.Product.price.toString()),
-          stock: parseInt(payload?.Product.stock.toString()),
-          category: payload?.Product.category,
-          image_url: payload?.Product.image_url,
-          quantity: payload?.Product.quantity,
-          created_at: new Date().toISOString(),
-          updated_at: new Date().toISOString(),
-        },
-      ])
-      .select()
-    if (data) {
-      return NextResponse.json(data, { status: 201 })
-    } else {
-      return NextResponse.json({ error }, { status: 404 })
+    const product = {
+      name: payload.Product.name,
+      description: payload.Product.description,
+      price: parseFloat(payload.Product.price.toString()),
+      stock: parseInt(payload.Product.stock.toString()),
+      category: payload.Product.category,
+      image_url: payload.Product.image_url,
+      quantity: parseInt(payload.Product.quantity.toString()),
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
     }
-  } catch (error) {
-    console.log('DATABASE ERROR', error)
+
+    const docRef = await addDoc(collection(db, 'products'), product)
+
+    return NextResponse.json({ id: docRef.id, ...product }, { status: 201 })
+  } catch (error: any) {
+    console.error('DATABASE ERROR', error)
+    return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
