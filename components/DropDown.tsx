cbe49ce4cfde6443599efdb3a9d5a8a20204461d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-04 17:47:39 +0500

    adding admin

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -1,30 +1,43 @@
+import { useUser } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const DropDown = () => {
+  const { user } = useUser()
   const Router = useRouter()
   return (
     <div className="absolute top-full mt-2 right-0 w-48 bg-green-200 border border-gray-200 rounded-lg shadow-lg z-10">
       <ul className="flex flex-col p-2">
-        {' '}
-        <li
-          onClick={() => Router.push('/Orders')}
-          className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
-        >
-          My Orders
-        </li>
-        <li
-          onClick={() => Router.push('/Favourites')}
-          className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
-        >
-          My Favourites
-        </li>
-        <li
-          onClick={() => Router.push('/Review')}
-          className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
-        >
-          My Reviews
-        </li>
+        {user?.primaryEmailAddress?.emailAddress ==
+        'hamzahussain14.hh@gmail.com' ? (
+          <li
+            onClick={() => Router.push('/Admin')}
+            className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+          >
+            Admin
+          </li>
+        ) : (
+          <>
+            <li
+              onClick={() => Router.push('/Orders')}
+              className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+            >
+              My Orders
+            </li>
+            <li
+              onClick={() => Router.push('/Favourites')}
+              className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+            >
+              My Favourites
+            </li>
+            <li
+              onClick={() => Router.push('/Review')}
+              className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+            >
+              My Reviews
+            </li>
+          </>
+        )}
       </ul>
     </div>
   )
