163e4d5910ee61027e8e104a0a11cca7a9d3c0bb
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-04 17:47:59 +0500

    removed unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 'use client'
 
 import GetProducts from '@/components/Product/GetProducts'
-import { useUser } from '@clerk/nextjs'
-import Image from 'next/image'
 
 export default function Home() {
-  const { user } = useUser()
-  console.log('USERRRR', user?.id)
-
   return (
     <>
       <GetProducts />
