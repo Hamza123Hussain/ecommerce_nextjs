830b61b621646ef0efd615818a05c4dcf9bc5541
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-07 04:34:49 +0500

    origin set

--- utils/Cors.ts
+++ utils/Cors.ts
@@ -4,7 +4,7 @@ import Cors from 'cors'
 // Initialize CORS middleware
 export const cors = Cors({
   methods: ['GET', 'POST', 'OPTIONS'],
-  origin: '*', // Adjust this to your frontend URL in production
+  origin: true, // Adjust this to your frontend URL in production
 })
 
 // Helper function to run middleware
