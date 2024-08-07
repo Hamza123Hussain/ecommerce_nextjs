4abc0112fc8fe2961d3f81f9afc220fbe3548fd5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-07 19:56:53 +0500

    fetching order from nodejs server

--- functions/Order/GetAllOrders.ts
+++ functions/Order/GetAllOrders.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const GetAllOrders = async () => {
   try {
-    const Response = await axios.get(`/api/Order/GetAll`)
+    const Response = await axios.get(`http://localhost:8000/api/Orders`)
     if (Response.status == 201) {
       console.log('from functions', Response.data)
       return Response.data
