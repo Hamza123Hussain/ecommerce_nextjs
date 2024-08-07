315b849fb66684b34b929ea3ccb791fe0a8aa6ec
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-07 19:39:43 +0500

    removing headers

--- app/api/Product/Get/route.ts
+++ app/api/Product/Get/route.ts
@@ -8,34 +8,16 @@ export const GET = async (req: any) => {
       console.log('Supabase error:', error)
       return new NextResponse(JSON.stringify({ error: error.message }), {
         status: 404,
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
       })
     }
     console.log('Fetched data from Supabase:', data)
     return new NextResponse(JSON.stringify(data), {
       status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
     })
   } catch (error) {
     console.log('Database error:', error)
     return new NextResponse(JSON.stringify({ error: 'Database error' }), {
       status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
     })
   }
 }
