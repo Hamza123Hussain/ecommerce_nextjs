e1fe00c8298e6d96965d46ba3bd99cde8ba6393d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-08 02:17:35 +0500

    made it responsive

--- components/Product/HomeProduct.tsx
+++ components/Product/HomeProduct.tsx
@@ -7,20 +7,20 @@ const HomeProduct = ({ product }: { product: Product }) => {
   const Router = useRouter()
   return (
     <div key={product.id} onClick={() => Router.push(`/Product/${product.id}`)}>
-      <div className="relative hover:shadow-md bg-gray-100 hover:shadow-gray-300 flex w-full max-w-xs flex-col overflow-hidden rounded-lg  shadow-md">
+      <div className="relative hover:shadow-md bg-white border-2 border-slate-200 hover:shadow-gray-300 flex w-full flex-col overflow-hidden rounded-lg  shadow-md">
         <img
           src={product.image_url}
           alt={product.name}
           className="rounded-t-lg  w-full h-[20vh] sm:h-[40vh]"
         />
 
-        <div className="mt-4 px-5 pb-5">
-          <h5 className="text-xl tracking-tight text-slate-900">
+        <div className=" mt-2 px-2">
+          <h5 className="text-xs sm:text-lg tracking-tight text-slate-900">
             {product.name}
           </h5>
 
-          <div className="mt-2 mb-5 flex w-full ">
-            <span className="text-3xl font-bold text-slate-900">
+          <div className="mt-2   w-full flex justify-end pb-2 ">
+            <span className="text-xs sm:text-lg font-bold text-slate-900">
               Rs {product.price}
             </span>
           </div>
